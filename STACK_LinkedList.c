7c9aea3ce0e21ad56287d891b949c2898079f6e4
Tanvir Ahammed <98035683+TANVIR-Ahammed-Bipul@users.noreply.github.com>
2023-09-26 23:20:16 +0600

    Add files via upload

--- STACK_LinkedList.c
+++ STACK_LinkedList.c
@@ -6,22 +6,20 @@ struct node
     int data;
     struct node *next;
 };
-
 typedef struct node node;
-
-int top = -1;
-node *newnode = NULL;
+node *top=NULL;
 
 void push(int n);
 void pop();
 void display();
+void peek();
 
 int main()
 {
     int n;
     printf("Enter size: ");
     scanf("%d", &n);
-    printf("\n\t1. Push\n\t2. Pop\n\t3. Display\n\t4. Exit\n");
+    printf("\n\t1. Push\n\t2. Pop\n\t3. Display\n\t4. Peek\n\t5. Exit\n");
 
     int choice;
     while (1)
@@ -41,6 +39,9 @@ int main()
             display();
             break;
         case 4:
+            peek();
+            break;
+        case 5:
             exit(0);
         default:
             printf("Invalid choice\n");
@@ -57,40 +58,40 @@ void push(int n)
     }
     else
     {
-        node *temp = (node *)malloc(sizeof(node));
-        if (temp == NULL)
+        node *newnode = (node *)malloc(sizeof(node));
+        if (newnode == NULL)
         {
             printf("Memory allocation failed\n");
             exit(1);
         }
         printf("\nEnter a number to push: ");
-        scanf("%d", &temp->data);
-        temp->next = newnode;
-        newnode = temp;
-        top++;
+        scanf("%d", &newnode->data);
+        newnode->next = top;
+        top = newnode;
     }
 }
 
 void pop()
 {
-    if (top == -1)
+    node *temp;
+    temp=top;
+    if (temp == NULL)
     {
-        printf("Stack is Empty\n");
+        printf("Underflow\n");
     }
     else
     {
-        node *temp = newnode;
         printf("Popped Element: %d\n", temp->data);
-        newnode = newnode->next;
+        top = top->next;
         free(temp);
-        temp--;
     }
 }
 
 void display()
 {
-    node *temp = newnode;
-    if (top == -1)
+    node *temp;
+    temp=top;
+    if (top == NULL)
     {
         printf("Stack is Empty\n");
     }
@@ -103,5 +104,17 @@ void display()
             temp = temp->next;
         }
         printf("\n");
+    }
+}
+
+void peek()
+{
+    if(top==NULL)
+    {
+        printf("\nTop is NULL.\n");
+    }
+    else
+    {
+        printf("\nPeek value is: %d\n", top->data);
     }
 }
